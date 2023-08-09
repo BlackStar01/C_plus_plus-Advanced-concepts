16a5ad48599eebd5787c51666b25ec4c6e4855af
Blackstar01 <vitalwarren@yahoo.com>
2023-08-09 12:40:33 +0200

    Tree insertion

--- test.cpp
+++ test.cpp
@@ -6,39 +6,62 @@ struct Node
     int value;
     Node *left_tree;
     Node *right_tree;
+    int deep;
 
     //intialize the node
-    Node(int val) : value(val), left_tree(nullptr), right_tree(nullptr) {};
+    Node(int val) : value(val), left_tree(nullptr), right_tree(nullptr), deep(0) {};
 };
 
 // Insert a new node
-void insert(const Node *my_tree, const int new_value) {
+Node *insert(Node *my_tree, const int new_value) {
+    
     if (my_tree == nullptr)
     {
+        cout << "Tree creation ..." << my_tree->value << endl;
         my_tree = new Node(new_value);
     }
+    Node *my_node = new Node(new_value);
+
+    auto print_step = [my_tree, new_value]() {
+        cout << "Inserting ... " << new_value << endl;
+        cout << "Parent node = "  << my_tree->value << endl << endl;
+    };
 
     if(new_value < my_tree->value) {
-        insert(my_tree->left_tree, new_value);
+        if(my_tree->left_tree == nullptr) {
+            print_step();
+            my_tree->left_tree = my_node;
+            return my_tree; 
+        }
+        insert(my_tree->left_tree, new_value); 
     }
     else if(new_value > my_tree->value) {
-        insert(my_tree->right_tree, new_value);
+        if(my_tree->right_tree == nullptr) {
+            print_step();
+            my_tree->right_tree = my_node;
+            return my_tree;
+        }
+        insert(my_tree->left_tree, new_value);
     }
-    else {
-        cout << "Elément existant" << endl;
+    else if(new_value == my_tree->value) {
+        print_step();
+        cout << "Élément existant..." << endl;
     }
+    return my_tree;
 }
 
 // Compute the deep of a binary tree ...
 int deep_tree(Node *tree) {
-    int compt = 0;
-
     if(tree->left_tree != nullptr) {
-        compt++;
+        tree->deep++;
+        deep_tree(tree->left_tree);
     }
     else if(tree->right_tree != nullptr) {
-        compt++;
+        tree->deep++;
+        deep_tree(tree->right_tree);
     }
+    cout << " Deep = " << tree->deep;
+    return tree->deep;
 }
 
 void display_node(Node *node) {
@@ -51,6 +74,19 @@ void display_binary_tree(Node *my_tree) {
 
 
 int main() {
+
+    Node *my_tree = new Node(8);
+
+    my_tree = insert(my_tree, 3);
+    my_tree = insert(my_tree, 10);
+    my_tree = insert(my_tree, 1);
+    my_tree = insert(my_tree, 6);
+    my_tree = insert(my_tree, 14);
+    my_tree = insert(my_tree, 4);
+    my_tree = insert(my_tree, 7);
+    my_tree = insert(my_tree, 13);
+
+    //cout << deep_tree(my_tree);
 
     return 0;
 }
